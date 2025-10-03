614dbf8d06d2d76a23f610a17bfaa679108d5db1
Freddy roberts <Freddyroberts69420@gmail.com>
2025-10-03 19:18:45 +1000

    create receiveAttack funcitonality

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,10 +1,25 @@
+function isObject(value) {
+  const type = typeof value;
+  return type === "object" && value !== null; // Exclude null, which typeof also returns as 'object'
+}
+
 export default class Gameboard {
   constructor() {
     this.board = Array.from({ length: 10 }, () => Array(10).fill(null));
   }
   placeShip(ship) {
     for (let i = 0; i < ship.spanXYsArr.length; i++) {
-      this.board[ship.spanXYsArr[i][0]][ship.spanXYsArr[i][1]] = "ship";
+      this.board[ship.spanXYsArr[i][0]][ship.spanXYsArr[i][1]] = ship;
+    }
+  }
+  receiveAttack(XY) {
+    const [x, y] = XY;
+
+    const cell = this.board[x][y];
+    if (isObject(cell)) {
+      cell.hit();
+    } else {
+      this.board[x][y] = "miss";
     }
   }
 }
--- unit-tests/gameboard.test.mjs
+++ unit-tests/gameboard.test.mjs
@@ -1,4 +1,5 @@
 import Gameboard from "../src/gameBoard";
+import Ship from "../src/ship.js";
 
 test("Gameboard constructor creates board 2d arr", () => {
 	const gameBoard = new Gameboard();
@@ -14,8 +15,34 @@ test("Gameboard places ship corresponding to ship.spanXYsARR", () => {
 		spanXYsArr: [ [0,0], [0,1], [0,2] ]
 	}
 	gameBoard.placeShip(ship);
-	expect(gameBoard.board[0][0]).toBe('ship');	
-	expect(gameBoard.board[0][1]).toBe("ship");	
-	expect(gameBoard.board[0][2]).toBe("ship");	
+	expect(gameBoard.board[0][0]).toBe(ship);	
+	expect(gameBoard.board[0][1]).toBe(ship);	
+	expect(gameBoard.board[0][2]).toBe(ship);	
 });
+
+test("Gameboard receiveAttack handles miss", () => {
+	const gameBoard = new Gameboard();	
+	gameBoard.receiveAttack([0,0]);
+	expect(gameBoard.board[0][0]).toBe('miss');
+});
+
+test("Gameboard receiveAttack handles hit", () => {
+	const gameBoard = new Gameboard();
+	const ship = {
+		length: 3,
+		sunk: false,
+		hits: 0,
+		spanXYsArr: [ [0,0], [0,1], [0,2] ]
+	}
+
+	ship.hit = function(){
+		ship.hits++;
+	};
+
+	gameBoard.placeShip(ship);
+	gameBoard.receiveAttack([0,0]);
+	expect(gameBoard.board[0][0].hits).toBe(1);
+});
+
+
 
