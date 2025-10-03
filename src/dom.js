c595010937c1dd83204739e0b2a2dd7e95a8cb34
Freddy roberts <Freddyroberts69420@gmail.com>
2025-10-03 23:04:07 +1000

    Start UI build, basic renderBoard function

--- src/dom.js
+++ src/dom.js
@@ -1,3 +1,20 @@
-const Dom = (() => {})();
+const Dom = (() => {
+  const renderBoard = (targetDiv) => {
+    const boardDiv = document.createElement("div");
+    boardDiv.className = "board-div";
+    for (let i = 0; i < 100; i++) {
+      const x = Math.floor(i / 10);
+      const y = i % 10;
+
+      const cell = document.createElement("div");
+      cell.id = `${x}${y}`;
+      cell.className = "board-cell";
+      boardDiv.append(cell);
+    }
+    document.querySelector(targetDiv).append(boardDiv);
+  };
+
+  return { renderBoard };
+})();
 
 export default Dom;
--- src/index.js
+++ src/index.js
@@ -3,3 +3,6 @@ import Gameboard from "./gameboard.js";
 import Player from "./player.js";
 import Dom from "./dom.js";
 import "./style.css";
+import "./reset.css";
+
+Dom.renderBoard(".p1-board-container");
